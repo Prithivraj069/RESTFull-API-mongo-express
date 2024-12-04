3547e0152066217fbf71ac024cf304f6465135af
Prithivraj Saravanan <prithivisaravanan069@gmail.com>
2024-12-04 02:19:40 +0000

    server started

--- express/index.js
+++ express/index.js
@@ -1,15 +1,47 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+const dbname = "recipe_book";
+
+const mongoUri = process.env.MONGO_URI;
+// const mongoUri = "mongodb+srv://root:prithiv069@cluster0.k9z8v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+console.log(mongoUri);
 
 let app = express();
+
+// !! Enable processing JSON data
+app.use(express.json());
+
+// !! Enable CORS
 app.use(cors());
 
-app.get('/', function(req, res) {
-    res.json({
-        "message": "Hello world"
+async function connect(uri, dbname) {
+    let client = await MongoClient.connect(uri, {
+        useUnifiedTopology: true
     })
+    _db = client.db(dbname);
+    return _db;
+}
+
+// SETUP END
+async function main() {
+
+  let db = await connect(mongoUri, dbname);
+
+  // Routes
+  app.get('/', function(req,res){
+    res.json({
+     "message":"Hello World!"
+   });
 })
 
-app.listen(3000, ()=>{
-    console.log("Server Started");
-})
+}
+
+main();
+
+// START SERVER
+app.listen(3000, () => {
+  console.log("Server has started");
+});
